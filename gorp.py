bb8f52ca16c96ecd454d75bbb9ed1c433411cff3
Jayant Shrivastava <jshrivastava03@gmail.com>
2022-01-22 17:20:54 -0500

    v1.0001

--- gorp.py
+++ gorp.py
@@ -1,7 +1,7 @@
 import subprocess
 
 def main():
-    proc = subprocess.Popen("open -a 'Google Chrome' 'https://www.youtube.com/watch?v=gwxTZaa3NgI'", shell=True)
+    proc = subprocess.Popen("open -a 'Google Chrome' 'https://www.youtube.com/watch?v=STlCxQge6ao'", shell=True)
     proc.communicate()
     
 if __name__ == "__main__":
